3d2943ab3a4960a0dd22656feb5cf136b878167d
guigs <guidentzien@hotmail.com>
2025-11-09 21:36:13 -0300

    Atualizando getters, setters e constructors

--- src/main/java/com/guigs/EntradaAtletas/entity/Atleta.java
+++ src/main/java/com/guigs/EntradaAtletas/entity/Atleta.java
@@ -1,9 +1,17 @@
 package com.guigs.EntradaAtletas.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Atleta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,70 +22,10 @@ public class Atleta {
     private boolean presente;
 
     @ManyToMany
-    private List<Treinador> treinadores;
-
     @JoinTable(
         name = "atleta_treinador",
         joinColumns = @JoinColumn(name = "atleta_id"),
         inverseJoinColumns = @JoinColumn(name = "treinador_id")
     )
-
-    //getters and setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<Treinador> getTreinadores() {
-        return treinadores;
-    }
-
-    public void setTreinadores(List<Treinador> treinadores) {
-        this.treinadores = treinadores;
-    }
-
-    public String getEquipe() {
-        return equipe;
-    }
-
-    public void setEquipe(String equipe) {
-        this.equipe = equipe;
-    }
-
-    public String getCor() {
-        return cor;
-    }
-
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
-
-    public boolean isPresente() {
-        return presente;
-    }
-
-    public void setPresente(boolean presente) {
-        this.presente = presente;
-    }
-
-    public Atleta() {}
-
-    public Atleta(List<Treinador> treinadores, String nome, String equipe, String cor, boolean presente) {
-        this.treinadores = treinadores;
-        this.nome = nome;
-        this.equipe = equipe;
-        this.cor = cor;
-        this.presente = presente;
-    }
+    private List<Treinador> treinadores;
 }
